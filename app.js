81b5ed283f4cac026ed983a77dd4e307ff9bc814
1 <tawers6103@gmail.com>
2021-10-01 15:46:39 +0300

    node

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
+
 const fs  = require('fs')
 const path = require('path')
 
-
-// const mkDir = path.join(__dirname,'users')
-// fs.mkdir ()
-const usersPath = path.join(__dirname,'users')
-const users = [
-    {name:"vasya", gender:'male',age:20},
-    {name:"vanya", gender:'male',age:20},
-    {name:"yura", gender:'male',age:20},
-    {name:"ihor", gender:'male',age:20},
-    {name:"marianna", gender:'female',age:20},
-    {name:"kate", gender:'female',age:20},
-    {name:"dasha", gender:'female',age:20},
-    {name:"valya", gender:'female',age:20},
-    {name:"masha", gender:'female',age:20},
-    {name:"georgiy", gender:'male',age:20}
-]
-function createUser(){
-    for (user of users){
-        if (user.gender === 'male'){
-            fs.writeFile(`boys/${user.name}.txt`, `${user.name} ${user.age}` ,(err)=>{
-                console.log(err)
-            })
+function gender(folderFrom,folderTO,gender) {
+    fs.readdir(path.join(__dirname,`${folderFrom}`), (err, files) => {
+        if (err) {
+            console.log(err);
         }
-        if (user.gender === 'female'){
-            fs.writeFile(`girls/${user.name}.txt`, `${user.name} ${user.age}` ,(err)=>{
-                console.log(err)
+        files.forEach(file => {
+            fs.readFile(path.join(__dirname,`${folderFrom}`,file), (err, data) => {
+                if(err) {
+                    return;
+                }
+                const parseUser = JSON.parse(data.toString())
+                console.log(parseUser)
+                if (parseUser.gender === gender){
+                    fs.rename(path.join(`${folderFrom}`, file), path.join(`${folderTO}`, file), (err) => {
+                        console.log(err);
+                    })
+                }
             })
-        }
-    }
+        })
+    })
 }
-createUser()
+gender('boys','girls','female')
+gender('girls','boys','male')
